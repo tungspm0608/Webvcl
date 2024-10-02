a2562f8527903a42dab06e78b55c5ffbaa9ea32d
PhungVanTung <tungspm0608@gmail.com>
2024-10-02 23:37:48 +0700

    create project

---
+++ src/main/java/com/example/webvcl/webvcl/WebvclApplication.java
@@ -0,0 +1,13 @@
+package com.example.webvcl.webvcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebvclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebvclApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webvcl/webvcl/WebvclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webvcl.webvcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebvclApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
